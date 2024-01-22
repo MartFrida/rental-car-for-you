656a75a689009d19e882107f7c5c96ecce65ebfd
Mary <32392607+MartFrida@users.noreply.github.com>
2024-01-22 03:26:33 +0200

    home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ const Header = () => {
   return (
     <>
       <header className='text-3xl py-4 bg-blue-950 flex justify-between px-4 fixed top-0 left-0 w-full z-10'>
-        <NavLink to='/' className='text-red-500 font-bold text-center  md:text-white lg:text-violet-600 hover:text-white transition-colors duration-300'>
+        <NavLink to='/' className='text-white font-bold text-center  transition-colors duration-300'>
           {'logo'}
         </NavLink>
         <div>
-          <NavLink className='text-white mr-6' to='/catalog'>
+          <NavLink className='text-white mr-6  transition-colors duration-300' to='/catalog'>
             Catalog
           </NavLink>
-          <NavLink className='text-white mr-6' to='/favorites'>
+          <NavLink className='text-white mr-6  transition-colors duration-300' to='/favorites'>
             Favorites
           </NavLink>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='bg-gradient-to-r from-cyan-500 to-blue-500 w-100% max-w-7xl p-0'>
+    <div className='bg-gradient-to-r from-cyan-500 to-blue-500 w-100% max-w-7xl p-0 relative'>
       <img className='object-cover ' src={'https://img.freepik.com/free-photo/aerial-view-of-road-in-mountains_335224-809.jpg?w=996&t=st=1705885278~exp=1705885878~hmac=9d13b6afb73d57658b7f47d39aed23fdbd9fb55bf9a1d18bcbe9e36f8fefa227'} alt='/buick_enclave' loading="lazy" />
+      <Link to='/catalog' className='absolute top-[100px] left-[100px] bg-blue-700 text-white  hover:bg-blue-100  p-5 rounded-xl'>Let's Go</Link>
     </div>
   )
 }
