7beb1b6c81d6301555f915810e1824430035ddf2
Mary <32392607+MartFrida@users.noreply.github.com>
2025-02-01 17:28:54 +0100

    styled header menu - burger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,99 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom"
 import logo from '../../img/logo.png'
-import { selectIsLoggedIn, selectUserName } from "../../redux/selectors"
+import { selectIsLoggedIn } from "../../redux/selectors"
 import { useDispatch, useSelector } from "react-redux"
 import { logoutThunk } from "../../redux/auth/operations"
 
 const Header = () => {
-  const userName = useSelector(selectUserName)
   const isLoggedIn = useSelector(selectIsLoggedIn)
+
   const dispatch = useDispatch()
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <>
-      <header className='text-3xl py-4 bg-sky-900/25 flex justify-between items-center px-4 fixed top-0 left-0 w-full z-10 backdrop-blur-sm p-4'>
+      <header className='text-3xl py-4 bg-sky-900/25 flex flex-wrap justify-between items-center px-4 fixed top-0 left-0 w-full z-10 backdrop-blur-sm p-4'>
         <NavLink to='/' className='text-white font-bold text-center transition-colors duration-300 '>
-          <img className="hidden md:flex h-auto w-36 md:w-40" src={logo} alt='logo' />
+          <img className=" h-auto w-36 md:w-40" src={logo} alt='logo' />
         </NavLink>
-        <div className="flex justify-between size-auto sm:gap-x-2 md:gap-x-6">
-          <NavLink className='text-white transition-colors duration-300 font-medium hover:font-bold' to='/catalog'>
-            Catalog
-          </NavLink>
-          <NavLink className='text-white  transition-colors duration-300 font-medium hover:font-bold' to='/favorites'>
-            Favorites
-          </NavLink>
-          {/* <NavLink to='/catalog/new' className='text-white  transition-colors duration-300 font-medium hover:font-bold'>Add</NavLink> */}
+        <div className="flex items-center lg:order-2">
           {!isLoggedIn &&
             <>
-              <NavLink to='/login' className='text-white  transition-colors duration-300 font-medium hover:font-bold'>Login</NavLink>
-              <NavLink to='/register' className='text-white transition-colors duration-300 font-medium hover:font-bold'>SignUp</NavLink>
-            </>
+              <NavLink to='/login' className="text-white hover:bg-blue-950  font-medium rounded-lg  px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Log in
+              </NavLink>
+              <NavLink to='/register' className="text-white hover:bg-blue-950  font-medium rounded-lg  px-4 lg:px-5 py-2 lg:py-2.5 mr-2">SignUp
+              </NavLink>
+            </>}
+          {isLoggedIn && <button onClick={() => dispatch(logoutThunk())} className="text-white hover:bg-blue-950  font-medium rounded-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Exit
+          </button>
           }
-          {isLoggedIn && <button onClick={() => dispatch(logoutThunk())} className='text-white transition-colors duration-300 font-medium hover:font-bold'>{`Exit ${userName}`}</button>}
         </div>
+        {/* Іконка бургер-меню для мобільних */}
+        <button
+          onClick={toggleMenu}
+          className="md:hidden focus:outline-none"
+          aria-label="Toggle Menu"
+        >
+          {isMenuOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          )}
+        </button>
+
+        {/* Навігаційне меню */}
+        <nav
+          className={`${isMenuOpen ? "block" : "hidden"
+            } md:flex md:items-center w-full md:w-auto`}
+        >
+          <ul className="flex flex-col md:flex-row md:space-x-6 mt-4 md:mt-0">
+            <li>
+              <NavLink to='/catalog'
+                className="block font-medium py-2 text-white hover:text-blue-950"
+              >
+                Catalog
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/favorites'
+                className="block font-medium py-2 text-white hover:text-blue-950"
+              >
+                Favorites
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import heroImg from '../../img/home-cars.png'
 
-
 const Home = () => {
   return (
     <div className='bg-gradient-to-r from-[#162556]  via-sky-500 via-70% to-[#162556]  h-[100vh] w-[100vw] relative font-semibold text-white overflow-hidden text-wrap p-4'>
       <img className='absolute top-[30%] left-0 object-cover w-[100%] ' src={heroImg} alt='/buick_enclave' loading="lazy" />
-      <div className=' absolute  my-20 top-10 right-0 flex p-4'>
-        <h1 className='uppercase text-center z-20 text-4xl ]'>Best CAR FOR RENT TODAY</h1>
+      <div className='w-full my-20 top-10 '>
+        <h1 className='uppercase z-20 text-4xl md:text-5xl font-bold text-center text-gray-900 mb-6 drop-shadow-lg'>Best CAR FOR RENT TODAY</h1>
+        <p className="hidden md:flex text-lg text-gray-200 mb-8 justify-center ">
+          Find the perfect car for your next adventure. Affordable, reliable, and ready to go!
+        </p>
       </div>
 
-      <Link to='/catalog' className='absolute bottom-5 right-5  bg-blue-950 text-white    transition  duration-300 ease-in-out hover:bg-butprimary  p-4 lg:p-5 font-semibold text-2xl rounded-xl'>Let's Go</Link>
-
+      <Link to='/catalog' className='absolute bottom-5 right-5 bg-sky-500 text-white transition duration-300 ease-in-out p-4 lg:p-5 font-semibold text-2xl rounded-lg hover:bg-blue-950  transition-colors'>Browse Cars</Link>
+      {/* bg-indigo-600 text-white px-6 py-3 rounded-lg hover:bg-indigo-700 transition-colors */}
     </div>
   )
 }
